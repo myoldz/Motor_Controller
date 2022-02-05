f545651357285cbd23bdc8f07ef564d2dbcf8114
Sal Polifemo <polifemo@comcast.net>
2022-02-05 11:28:30 -0500

    Addes potentiometer

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,43 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Define LCD I2C address
-const int i2c_addr = 0x27;
+
+const int i2c_addr = 0x27;    //LCD I2C address
 
 #define POT_PIN A3
 
+// Global variables
+long potValNew = 0.0;
+long potValOld = 0.0;
 
-LiquidCrystal_I2C lcd(i2c_addr, 20, 4);
+
+// Initialize stuff
+LiquidCrystal_I2C lcd(i2c_addr, 16, 2);
 
 void setup() {
   Serial.begin(9600);
-  /*
+  
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  potValNew = analogRead(POT_PIN);
+  if(potValNew > potValOld * 1.05 || potValNew < potValOld * 0.95){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Pot vlaue > ");
+    lcd.print(potValNew);
+   }
+
+  delay(1000);
+  
+}
+
+void i2cList(){
+  
+  // Use this code to find the address of the I2C devices attached
+
   Serial.println("Scanning I2C bus for LCD");
   byte count = 0;
   Wire.begin();
@@ -32,16 +58,5 @@ void setup() {
   Serial.print("Found ");
   Serial.print(count, DEC);
   Serial.println(" device(s).");
-  */
-
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(3,0);
-  lcd.print("Hello");
-  lcd.setCursor(2,1);
-  lcd.print("Arduino!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
